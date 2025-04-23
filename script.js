1a681a337683d8598a2a27d306c7b4c1e739c549
Kaan Eroltu <kaaneroltu73@gmail.com>
2025-04-23 13:02:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p', { origin: 'right' });
 ScrollReveal().reveal('.about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Student', 'Computer Scientist', 'Electrical Engineering'],
+    strings: ['Student', 'Computer Scientist', 'Electrical Engineer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 100,
